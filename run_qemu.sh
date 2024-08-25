c7b28c070f7d2bbc4428cc78cb019df9beebcdd1
fabius <witherbones1@gmail.com>
2024-08-25 14:45:17 +0200

    Cleaned iso_content

--- run_qemu.sh
+++ run_qemu.sh
@@ -1,3 +1,4 @@
 cargo build &&
+mkdir -p iso_content/EFI/BOOT &&
 cp target/x86_64-unknown-uefi/debug/meadowfoam.efi img_content/EFI/BOOT/BOOTX64.efi &&
 qemu-system-x86_64 -net none --bios $OVMF_FD -m 256M -hdb fat:rw:img_content/ -s #-S
